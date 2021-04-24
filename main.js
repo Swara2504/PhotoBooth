f689195a69251b024bf3d258f2a0242e72462503
Swara2504 <75523745+Swara2504@users.noreply.github.com>
2021-04-24 15:57:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload()
+{
+}
+function setup()
+{
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+}
+function draw()
+{
+}
+function takeSnapshot()
+{
+ save('photo_booth_Image.png');
+}
